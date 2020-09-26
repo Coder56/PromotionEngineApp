740f9d8c249711069ecc788d87ff7aea30b9fda6
Coder56 <59683199+Coder56@users.noreply.github.com>
2020-09-26 15:18:13 +0530

    changed code to modular approach and added main project files with changes

---
+++ PromotionEngine/Model/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product getProduct(string ProductId)
+        {
+            Product product = new Product();            
+            switch (ProductId)
+            {
+                case "A":
+                    product.Id = ProductId;
+                    product.Price = 50;                    
+
+                    break;
+                case "B":
+                    product.Id = ProductId;
+                    product.Price = 30;                    
+
+                    break;
+                case "C":
+                    product.Id = ProductId;
+                    product.Price = 20;                    
+
+                    break;
+                case "D":
+                    product.Id = ProductId;
+                    product.Price = 15;                    
+                    break;
+            }
+            return product;
+        }
+
+    }
+}
+
---
+++ PromotionEngine/PriceCalculations/Price.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class Price
+    {
+        public int GetTotalPrice(List<Product> products)
+        {
+            int counterofA = 0, counterofB = 0, CounterofC = 0, CounterofD = 0;
+            
+            int priceofA = 50, priceofB = 30, priceofC = 20, priceofD = 15;           
+
+            foreach (Product pr in products)
+            {
+                if (pr.Id == "A")
+                    counterofA = counterofA + 1;
+
+                else if (pr.Id == "B")
+                    counterofB = counterofB + 1;
+
+                else if (pr.Id == "C")
+                    CounterofC = CounterofC + 1;
+
+                else if (pr.Id == "D")
+                    CounterofD = CounterofD + 1;
+            }
+
+            int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
+            int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
+
+            if (CounterofC >= 1 && CounterofD >= 1)
+            {
+                int totalPriceofCandD = 0;
+
+                if (CounterofC == CounterofD)
+                    totalPriceofCandD = CounterofC * 30;
+                else
+                    totalPriceofCandD = CounterofC > CounterofD ?
+                                                CounterofD * 30 + (CounterofC - CounterofD) * 20
+                                                : CounterofC * 30 + (CounterofD - CounterofC) * 15;
+
+                return totalPriceofA + totalPriceofB + totalPriceofCandD;                
+            }
+
+            int totalPriceofC = (CounterofC * priceofC);
+            int totalPriceofD = (CounterofD * priceofD);
+
+            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD;            
+        }
+    }
+}
